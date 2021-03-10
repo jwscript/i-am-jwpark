149308c2bc779552e23a89500b97c1c25c833ac2
jwpark <wincheran@gmail.com>
2021-03-10 15:19:20 +0900

    test 1

--- src/main/java/net/jwpark/domain/Result.java
+++ src/main/java/net/jwpark/domain/Result.java
@@ -2,33 +2,30 @@ package net.jwpark.domain;
 
 public class Result {
 	private boolean valid;
-	
+
 	private String errorMessage;
-	
-	public boolean isValid()
-	{
+
+	public boolean isValid() {
 		return this.valid;
 	}
-	
-	public String getErrorMessage()
-	{
+
+	public String getErrorMessage() {
 		return this.errorMessage;
 	}
-	
-	
+
 	private Result() {
 		this(true, null);
 	}
-	
+
 	private Result(boolean valid, String errorMessage) {
 		this.valid = valid;
 		this.errorMessage = errorMessage;
 	}
-	
+
 	public static Result ok() {
 		return new Result();
 	}
-	
+
 	public static Result fail(String errorMessage) {
 		return new Result(false, errorMessage);
 	}
